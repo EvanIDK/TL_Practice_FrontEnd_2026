55a5501b48d39e8fcd568c2653698c60534f3ce7
Иван <gerasimov_ivan2006@mail.ru>
2026-06-03 22:32:00 +0300

    обновил eslint, поправил WARNING

--- client/src/tests/components/App.test.tsx
+++ client/src/tests/components/App.test.tsx
@@ -7,66 +7,59 @@ describe('App', () => {
     const SelectLenght = 5;
 
     it('hides the MoreAbout when changing the currency', async () => {
+        render(<App />)
 
-    render(<App />)
+        const button = screen.getByTestId('more-about-button')
+        await userEvent.click(button)
 
-    const button = screen.getByTestId('more-about-button')
-    await userEvent.click(button)
-
-    expect(screen.getByTestId('more-about-from-content')).toBeInTheDocument()
-    expect(screen.getByTestId('more-about-to-content')).toBeInTheDocument()
+        expect(screen.getByTestId('more-about-from-content')).toBeInTheDocument()
+        expect(screen.getByTestId('more-about-to-content')).toBeInTheDocument()
 
 
-    const select = screen.getByTestId('currency-select-from-select')
-    await userEvent.selectOptions(select, 'CAD')
+        const select = screen.getByTestId('currency-select-from-select')
+        await userEvent.selectOptions(select, 'CAD')
 
-    expect(screen.queryByTestId('more-about-from-content')).not.toBeInTheDocument()
-    expect(screen.queryByTestId('more-about-to-content')).not.toBeInTheDocument()
-
+        expect(screen.queryByTestId('more-about-from-content')).not.toBeInTheDocument()
+        expect(screen.queryByTestId('more-about-to-content')).not.toBeInTheDocument()
     })
 
     it('It does not allow you to select a single currency pair', async () => {
+        render(<App />)
 
-    render(<App />)
+        const selectFrom = screen.getByTestId('currency-select-from-select')
+        await userEvent.selectOptions(selectFrom, 'CAD')
+        const selectTo = screen.getByTestId('currency-select-to-select')
+        await userEvent.selectOptions(selectTo, 'CAD')
 
-    const selectFrom = screen.getByTestId('currency-select-from-select')
-    await userEvent.selectOptions(selectFrom, 'CAD')
-    const selectTo = screen.getByTestId('currency-select-to-select')
-    await userEvent.selectOptions(selectTo, 'CAD')
-
-    expect(screen.queryByTestId('currency-select-from-select')).not.toHaveValue('CAD')
-    expect(selectTo).toHaveValue('CAD')
-
+        expect(screen.queryByTestId('currency-select-from-select')).not.toHaveValue('CAD')
+        expect(selectTo).toHaveValue('CAD')
     })
 
     it('recalculation of conversion when changing the amount/pair', async () => {
+        const EXPECTED_CAD_TO_PLN_RESULT = 22.125;
+        render(<App />)
 
-    render(<App />)
+        const selectFrom = screen.getByTestId('currency-select-from-select')
+        await userEvent.selectOptions(selectFrom, 'CAD')
+        const selectTo = screen.getByTestId('currency-select-to-select')
+        await userEvent.selectOptions(selectTo, 'PLN')
 
-    const selectFrom = screen.getByTestId('currency-select-from-select')
-    await userEvent.selectOptions(selectFrom, 'CAD')
-    const selectTo = screen.getByTestId('currency-select-to-select')
-    await userEvent.selectOptions(selectTo, 'PLN')
+        const inputFrom = screen.getByTestId('currency-select-from-input')
+        await userEvent.clear(inputFrom)
+        await userEvent.type(inputFrom, '7.5')
 
-    const inputFrom = screen.getByTestId('currency-select-from-input')
-    await userEvent.clear(inputFrom)
-    await userEvent.type(inputFrom, '7.5')
-
-    expect(screen.getByTestId('currency-select-to-input')).toHaveValue(22.125)
-
+        expect(screen.getByTestId('currency-select-to-input')).toHaveValue(EXPECTED_CAD_TO_PLN_RESULT)
     })
 
     it('rendering of selections and fields with mock data', async () => {
 
-    render(<App />)
+        render(<App />)
 
-    const selectFrom = screen.getByTestId('currency-select-from-select')
-    const options = within(selectFrom).getAllByRole('option')
-    expect(options).toHaveLength(SelectLenght)
+        const selectFrom = screen.getByTestId('currency-select-from-select')
+        const options = within(selectFrom).getAllByRole('option')
+        expect(options).toHaveLength(SelectLenght)
 
-    const inputFrom = screen.getByTestId('currency-select-from-input')
-    expect(inputFrom).toHaveValue(1)
-
+        const inputFrom = screen.getByTestId('currency-select-from-input')
+        expect(inputFrom).toHaveValue(1)
     })
-
 })
